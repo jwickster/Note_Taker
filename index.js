754711a0b782dadac5487506d84a7d89ece93df4
Josh Wickster <joshwickster@gmail.com>
2020-12-01 10:54:53 -0700

    Added edits

---
+++ index.js
@@ -0,0 +1,33 @@
+var $noteTitle = $(".note-title");
+var $noteText = $(".note-textarea");
+var $saveNoteBtn = $(".save-note");
+var $newNoteBtn = $(".new-note");
+var $noteList = $(".list-container .list-group");
+
+let activeNote = {};
+
+//pull notes from database
+var getNotes = () => {
+  return $.ajax({
+    url: '/api/notes',
+    method: 'GET'
+  });
+};
+
+//save note to db
+var saveNote = () => {
+  return $.ajax({
+    url: '/api/notes',
+    data: none,
+    method: 'POST'
+  });
+};
+
+//remove note from db
+var deleteNote = () => {
+  return $.ajax({
+    url: '/api/notes' + id,
+    method: 'DELETE'
+  });
+};
+
